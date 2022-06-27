cee8c5aa33441ee1a8fbd58b2b981435ffd95aba
unknown <https://github.com/Pooja-bhaskaran/Day03.git>
2022-06-27 19:52:16 +0530

    Push operation

--- src/com/Bridgelabz/woekshop3/Main.java
+++ src/com/Bridgelabz/woekshop3/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         LinkedList<Integer> list1 = new LinkedList();
-        System.out.println("PUSH");
+        System.out.println("PUSH OPERATION");
         list1.push(20);
         list1.push(40);
         list1.push(60);
